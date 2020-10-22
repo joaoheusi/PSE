d6c7f11bd63a662af26815e3cf0948016d4d5271
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2020-10-21 22:59:25 -0300

    WEB APP ok

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from logic import write_json,read_json,get_latest_status
 from datetime import datetime
-
+from flask import request
 app = Flask(__name__)
 
 
@@ -15,20 +15,28 @@ def index():
     return  render_template('index.html')
 
 
-@app.route('/aplicacao')
+@app.route('/aplicacao', methods=['GET','POST'])
 def main_app():
-    estado = get_latest_status()
-    context = estado
-    button_name = estado.get("button_name")
-    button_class = estado.get("button_class")
-    button_url = "/aplicacao"
-    write_info = estado.get("write_info")
-    print(write_info)
-    if write_info is None:
-        pass
-    else:
+    if request.method == 'POST':
+        estado = get_latest_status()
+        write_info = estado.get("write_info") 
         write_json(write_info)
-    return  render_template('aplicacao.html', button_class = button_class, button_name = button_name, button_url =button_url)
+        estado = get_latest_status()
+        context = estado
+        button_name = estado.get("button_name")
+        button_class = estado.get("button_class")
+        button_url = "main_app"
+
+        return render_template('aplicacao.html', button_class = button_class, button_name = button_name, button_url =button_url)
+     
+    if request.method == 'GET':
+        estado = get_latest_status()
+        context = estado
+        button_name = estado.get("button_name")
+        button_class = estado.get("button_class")
+        button_url = "main_app"
+
+        return render_template('aplicacao.html', button_class = button_class, button_name = button_name, button_url =button_url)
 
 @app.route('/historico')
 def history():
